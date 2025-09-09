63edd41c441be8d0b2e2de739f12253723cad173
italeo <ishmaeltaleo@gmail.com>
2025-09-08 17:57:18 -0700

    Working on permute function

--- src/permute.rs
+++ src/permute.rs
@@ -1,5 +1,9 @@
 pub fn permute(input: u64, table: &[u8], in bits: usize) -> 64 {
     // DES tables are 1-based and MSB-first.
-    
+    let mut out = 0u64;
+
+    for (i, &pos1) in table.iter().enumerate() {
+        let src = in_bits - (pos1 as usize)
+    }
 
 }
